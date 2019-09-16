ab896f33891301fd3916fff41d420bfbf2d011cb
Bo Zhang <zhangbodut@gmail.com>
2019-09-16 14:54:30 +0800

    Initial commit

---
+++ src/main/java/com/github/hcsp/algorithm/BinaryTree.java
@@ -0,0 +1,46 @@
+package com.github.hcsp.algorithm;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BinaryTree {
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        TreeNode node6 = new TreeNode(6);
+
+        node1.left = node2;
+        node1.right = node3;
+
+        node2.left = node4;
+        node2.right = node5;
+
+        node3.right = node6;
+
+        System.out.println(bfs(node1));
+        System.out.println(dfs(node1));
+    }
+
+    // 请实现二叉树的广度优先遍历（层次遍历）
+    public static List<Integer> bfs(TreeNode root) {
+        return Collections.emptyList();
+    }
+
+    // 请实现二叉树的深度优先遍历（前序）
+    public static List<Integer> dfs(TreeNode root) {
+        return Collections.emptyList();
+    }
+
+    public static class TreeNode {
+        int value;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int value) {
+            this.value = value;
+        }
+    }
+}
