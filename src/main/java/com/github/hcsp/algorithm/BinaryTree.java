1db1eb217bace77cf5a70eefcb3b1443a6bf948c
Ben <50296011+MaoYuanFang@users.noreply.github.com>
2019-11-01 11:44:05 +0800

    Update BinaryTree.java (#12)
    Update BinaryTree.java (#12)

--- src/main/java/com/github/hcsp/algorithm/BinaryTree.java
+++ src/main/java/com/github/hcsp/algorithm/BinaryTree.java
@@ -1,7 +1,13 @@
 package com.github.hcsp.algorithm;
 
-import java.util.Collections;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
+import java.util.LinkedList;
+import java.util.Queue;
+
 
 public class BinaryTree {
     public static void main(String[] args) {
@@ -26,12 +32,40 @@ public class BinaryTree {
 
     // 请实现二叉树的广度优先遍历（层次遍历）
     public static List<Integer> bfs(TreeNode root) {
-        return Collections.emptyList();
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode poll = queue.poll();
+            list.add(poll.value);
+            addTreeNode(poll.left, queue);
+            addTreeNode(poll.right, queue);
+        }
+        return list;
+    }
+
+    private static void addTreeNode(TreeNode node, Queue<TreeNode> queue) {
+        if (node != null) {
+            if (queue instanceof ArrayDeque) {
+                ((ArrayDeque<TreeNode>) queue).push(node);
+            } else {
+                queue.add(node);
+            }
+        }
     }
 
     // 请实现二叉树的深度优先遍历（前序）
     public static List<Integer> dfs(TreeNode root) {
-        return Collections.emptyList();
+        ArrayList<Integer> list = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode pop = stack.pop();
+            list.add(pop.value);
+            addTreeNode(pop.right, stack);
+            addTreeNode(pop.left, stack);
+        }
+        return list;
     }
 
     public static class TreeNode {
