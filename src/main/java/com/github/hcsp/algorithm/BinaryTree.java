f06c637a94daaf65dc121472daf60b6439467238
Kisu <1315684099@qq.com>
2019-11-05 10:17:50 +0800

    è练习二叉树遍历

--- src/main/java/com/github/hcsp/algorithm/BinaryTree.java
+++ src/main/java/com/github/hcsp/algorithm/BinaryTree.java
@@ -1,7 +1,8 @@
 package com.github.hcsp.algorithm;
 
-import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class BinaryTree {
     public static void main(String[] args) {
@@ -22,16 +23,48 @@ public class BinaryTree {
 
         System.out.println(bfs(node1));
         System.out.println(dfs(node1));
+        System.out.println(ldr(node1));
     }
 
     // 请实现二叉树的广度优先遍历（层次遍历）
     public static List<Integer> bfs(TreeNode root) {
-        return Collections.emptyList();
+        List<Integer> list = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        if (root == null) {
+            return list;
+        }
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode t = queue.remove();
+
+            if (t.left != null) {
+                queue.add(t.left);
+            }
+            if (t.right != null) {
+                queue.add(t.right);
+            }
+            list.add(t.value);
+        }
+        return list;
     }
 
     // 请实现二叉树的深度优先遍历（前序）
     public static List<Integer> dfs(TreeNode root) {
-        return Collections.emptyList();
+        List<Integer> list = new LinkedList<>();
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode pNode = root;
+
+        while (pNode != null || !stack.isEmpty()) {
+            if (pNode != null) {
+                list.add(pNode.value);
+                stack.push(pNode);
+                pNode = pNode.left;
+            } else {
+                pNode = stack.pop().right;
+            }
+        }
+        return list;
     }
 
     public static class TreeNode {
@@ -42,5 +75,24 @@ public class BinaryTree {
         public TreeNode(int value) {
             this.value = value;
         }
+    }
+    // 请实现二叉树的深度优先遍历（中序）
+
+    public static List<Integer> ldr(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode pNode = root;
+
+        while (pNode != null || !stack.isEmpty()) {
+            if (pNode != null) {
+                stack.push(pNode);
+                pNode = pNode.left;
+            } else {
+                TreeNode node = stack.pop();
+                list.add(node.value);
+                pNode = node.right;
+            }
+        }
+        return list;
     }
 }
