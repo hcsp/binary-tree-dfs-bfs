6cdc53063da3c7ce97c8578fc4db2811a618b245
XiongAmao <onycat@live.cn>
2020-02-05 16:52:38 +0800

    Binary Tree bfs/dfs practices. (#35)
    Binary Tree bfs/dfs practices. (#35)

--- src/main/java/com/github/hcsp/algorithm/BinaryTree.java
+++ src/main/java/com/github/hcsp/algorithm/BinaryTree.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.algorithm;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class BinaryTree {
@@ -26,12 +27,40 @@ public class BinaryTree {
 
     // 请实现二叉树的广度优先遍历（层次遍历）
     public static List<Integer> bfs(TreeNode root) {
-        return Collections.emptyList();
+        List<Integer> result = new ArrayList<>();
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.push(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.remove();
+            result.add(node.value);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        return result;
     }
 
     // 请实现二叉树的深度优先遍历（前序）
     public static List<Integer> dfs(TreeNode root) {
-        return Collections.emptyList();
+        List<Integer> result = new ArrayList<>();
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        TreeNode curNode = root;
+
+        while (curNode != null || !stack.isEmpty()) {
+            if (curNode != null) {
+                result.add(curNode.value);
+                stack.push(curNode);
+                curNode = curNode.left;
+            } else {
+                curNode = stack.pop().right;
+            }
+        }
+
+        return result;
     }
 
     public static class TreeNode {
