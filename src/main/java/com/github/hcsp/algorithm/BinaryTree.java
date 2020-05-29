69e3e5be477f8147d045f979b2616260443b1f4a
Sunpeng526 <55687152+Sunpeng526@users.noreply.github.com>
2020-05-29 18:03:08 +0800

    depth and board first search (#68)
    depth and board first search (#68)

--- src/main/java/com/github/hcsp/algorithm/BinaryTree.java
+++ src/main/java/com/github/hcsp/algorithm/BinaryTree.java
@@ -1,7 +1,10 @@
 package com.github.hcsp.algorithm;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
 
 public class BinaryTree {
     public static void main(String[] args) {
@@ -24,14 +27,44 @@ public class BinaryTree {
         System.out.println(dfs(node1));
     }
 
-    // 请实现二叉树的广度优先遍历（层次遍历）
+    /*
+     * 请实现二叉树的广度优先遍历（层次遍历）
+     */
     public static List<Integer> bfs(TreeNode root) {
-        return Collections.emptyList();
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            result.add(node.value);
+            if (null != node.left) {
+                queue.add(node.left);
+            }
+            if (null != node.right) {
+                queue.add(node.right);
+            }
+        }
+        return result;
     }
 
-    // 请实现二叉树的深度优先遍历（前序）
+    /*
+     * 请实现二叉树的深度优先遍历（前序）
+     */
     public static List<Integer> dfs(TreeNode root) {
-        return Collections.emptyList();
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.value);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return result;
     }
 
     public static class TreeNode {
