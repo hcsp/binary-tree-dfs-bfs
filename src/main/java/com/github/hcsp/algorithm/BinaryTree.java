b36025a680c34cbefb10b9bc17d22b56df5335fc
cyunt <chenyuntao1997@163.com>
2020-08-19 13:35:20 +0800

    dfsbfs

--- src/main/java/com/github/hcsp/algorithm/BinaryTree.java
+++ src/main/java/com/github/hcsp/algorithm/BinaryTree.java
@@ -1,9 +1,14 @@
 package com.github.hcsp.algorithm;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BinaryTree {
+    static Set<Integer> set = new HashSet<>();
+    static List<Integer> list = new ArrayList<>();
+
     public static void main(String[] args) {
         TreeNode node1 = new TreeNode(1);
         TreeNode node2 = new TreeNode(2);
@@ -26,12 +31,34 @@ public class BinaryTree {
 
     // 请实现二叉树的广度优先遍历（层次遍历）
     public static List<Integer> bfs(TreeNode root) {
-        return Collections.emptyList();
+//        System.out.println(root.value);
+        set.add(root.value);
+        if (root.left != null) {
+            set.add(root.left.value);
+        }
+        if (root.right != null) {
+            set.add(root.right.value);
+        }
+        if (root.left != null) {
+            bfs(root.left);
+        }
+        if (root.right != null) {
+            bfs(root.right);
+        }
+        return new ArrayList<>(set);
     }
 
     // 请实现二叉树的深度优先遍历（前序）
     public static List<Integer> dfs(TreeNode root) {
-        return Collections.emptyList();
+//        System.out.println(root.value);
+        list.add(root.value);
+        if (root.left != null) {
+            dfs(root.left);
+        }
+        if (root.right != null) {
+            dfs(root.right);
+        }
+        return list;
     }
 
     public static class TreeNode {
