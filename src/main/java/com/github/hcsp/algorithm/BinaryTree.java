11520af10c88c02e17df5821d3dfee869a633277
mushan <49012328+mushanwb@users.noreply.github.com>
2020-09-04 11:22:33 +0800

    广度优先遍历 和 深度优先遍历 (#86)
    广度优先遍历 和 深度优先遍历 (#86)

--- src/main/java/com/github/hcsp/algorithm/BinaryTree.java
+++ src/main/java/com/github/hcsp/algorithm/BinaryTree.java
@@ -1,7 +1,6 @@
 package com.github.hcsp.algorithm;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class BinaryTree {
     public static void main(String[] args) {
@@ -11,6 +10,12 @@ public class BinaryTree {
         TreeNode node4 = new TreeNode(4);
         TreeNode node5 = new TreeNode(5);
         TreeNode node6 = new TreeNode(6);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node8 = new TreeNode(8);
+        TreeNode node9 = new TreeNode(9);
+        TreeNode node10 = new TreeNode(10);
+
+
 
         node1.left = node2;
         node1.right = node3;
@@ -20,18 +25,64 @@ public class BinaryTree {
 
         node3.right = node6;
 
-        System.out.println(bfs(node1));
+        node4.left = node8;
+        node4.right = node9;
+
+        node6.left = node7;
+        node6.right = node10;
+
+//        System.out.println(bfs(node1));
         System.out.println(dfs(node1));
     }
 
     // 请实现二叉树的广度优先遍历（层次遍历）
     public static List<Integer> bfs(TreeNode root) {
-        return Collections.emptyList();
+        List<Integer> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        // 使用队列存储树节点
+        while (queue.size() != 0) {
+            TreeNode tempRoot = queue.poll();
+            list.add(tempRoot.value);
+            if (tempRoot.left != null) {
+                queue.add(tempRoot.left);
+            }
+            if (tempRoot.right != null) {
+                queue.add(tempRoot.right);
+            }
+        }
+        return list;
     }
+
+
 
     // 请实现二叉树的深度优先遍历（前序）
     public static List<Integer> dfs(TreeNode root) {
-        return Collections.emptyList();
+        List<Integer> list = new ArrayList<>();
+        // 使用栈（双端队列）
+        Deque<TreeNode> deque = new LinkedList<>();
+        deque.add(root);
+        list.add(root.value);
+        boolean flag = true;
+        while (deque.size() != 0) {
+            TreeNode tempNode = deque.peekLast();
+            if (tempNode.left != null && flag) {
+                TreeNode leftNode = tempNode.left;
+                list.add(leftNode.value);
+                deque.add(leftNode);
+            } else {
+                deque.pollLast();
+                if (tempNode.right != null) {
+                    TreeNode rightNode = tempNode.right;
+                    list.add(rightNode.value);
+                    deque.add(rightNode);
+                    flag = true;
+                } else {
+                    flag = false;
+                }
+            }
+        }
+        return list;
     }
 
     public static class TreeNode {
