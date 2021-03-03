96b0a0c5423dc3a6e794106ffd6d88320828a4bc
pccmast <248981941@qq.com>
2021-03-03 15:24:28 +0800

    实现dfs、bfs

--- src/main/java/com/github/hcsp/algorithm/BinaryTree.java
+++ src/main/java/com/github/hcsp/algorithm/BinaryTree.java
@@ -1,5 +1,6 @@
 package com.github.hcsp.algorithm;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -26,12 +27,45 @@ public class BinaryTree {
 
     // 请实现二叉树的广度优先遍历（层次遍历）
     public static List<Integer> bfs(TreeNode root) {
-        return Collections.emptyList();
+        ArrayList<TreeNode> treeNodes = new ArrayList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return Collections.emptyList();
+        } else {
+            treeNodes.add(root);
+            result.add(root.value);
+            while (!treeNodes.isEmpty()) {
+                TreeNode tempNode = treeNodes.remove(0);
+                if (tempNode.left != null) {
+                    treeNodes.add(tempNode.left);
+                    result.add(tempNode.left.value);
+                }
+                if (tempNode.right != null) {
+                    treeNodes.add(tempNode.right);
+                    result.add(tempNode.right.value);
+                }
+            }
+        }
+        return result;
+    }
+
+    private static void recursive_dfs(TreeNode treeNode, ArrayList<Integer> intergers) {
+        if (treeNode != null) {
+            intergers.add(treeNode.value);
+            recursive_dfs(treeNode.left, intergers);
+            recursive_dfs(treeNode.right, intergers);
+        }
     }
 
     // 请实现二叉树的深度优先遍历（前序）
     public static List<Integer> dfs(TreeNode root) {
-        return Collections.emptyList();
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return Collections.emptyList();
+        } else {
+            recursive_dfs(root, result);
+        }
+        return result;
     }
 
     public static class TreeNode {
