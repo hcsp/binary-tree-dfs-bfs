ecede36bed1d3f5c06671e8743a5163b16fad94c
Zhouzf <aaaa159557190@gmail.com>
2021-09-10 15:11:53 +0800

    使用栈，队列，递归完成二叉树遍历

--- src/main/java/com/github/hcsp/algorithm/BinaryTree.java
+++ src/main/java/com/github/hcsp/algorithm/BinaryTree.java
@@ -1,7 +1,6 @@
 package com.github.hcsp.algorithm;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class BinaryTree {
     public static void main(String[] args) {
@@ -21,18 +20,70 @@ public class BinaryTree {
         node3.right = node6;
 
         System.out.println(bfs(node1));
-        System.out.println(dfs(node1));
+        System.out.println(dfs2(node1));
     }
+
 
     // 请实现二叉树的广度优先遍历（层次遍历）
     public static List<Integer> bfs(TreeNode root) {
-        return Collections.emptyList();
+        if (root == null) {
+            return Collections.emptyList();
+        }
+
+        Queue<TreeNode> treeNodeQueue = new LinkedList<>();
+        treeNodeQueue.add(root);
+
+        List<Integer> result = new ArrayList<>();
+        while (!treeNodeQueue.isEmpty()) {
+            TreeNode node = treeNodeQueue.poll();
+            result.add(node.value);
+
+            if (node.left != null) {
+                treeNodeQueue.add(node.left);
+            }
+            if (node.right != null) {
+                treeNodeQueue.add(node.right);
+            }
+        }
+        return result;
     }
 
-    // 请实现二叉树的深度优先遍历（前序）
+    // 请实现二叉树的深度优先遍历（前序，递归实现）
     public static List<Integer> dfs(TreeNode root) {
-        return Collections.emptyList();
+        // 根 --> 左 --> 右
+        List<Integer> result = new ArrayList<>();
+        if (root != null) {
+            result.add(root.value);
+            result.addAll(dfs(root.left));
+            result.addAll(dfs(root.right));
+        }
+        return result;
     }
+
+    // 请实现二叉树的深度优先遍历（前序，Stack 实现）
+    public static List<Integer> dfs2(TreeNode root) {
+        if (root == null) {
+            return Collections.emptyList();
+        }
+
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+
+        List<Integer> result = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            TreeNode pollNode = stack.pop();
+            result.add(pollNode.value);
+            if (pollNode.right != null) {
+                stack.push(pollNode.right);
+            }
+            if (pollNode.left != null) {
+                stack.push(pollNode.left);
+            }
+        }
+
+        return result;
+    }
+
 
     public static class TreeNode {
         int value;
